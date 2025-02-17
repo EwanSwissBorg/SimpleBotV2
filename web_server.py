47b9c52c8f11780c9dc80ac4aebd5940dca661c2
Ewan Hamon <ewan.hamon@swissborg.com>
2025-02-17 13:55:15 +0100

    feat: :rocket: update web_server to run in global

--- web_server.py
+++ web_server.py
@@ -52,4 +52,5 @@ def twitter_callback():
         return f"Erreur détaillée : {str(e)}"
 
 if __name__ == '__main__':
-    app.run(port=5000) 
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port) 
